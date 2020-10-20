0b5b2ebfe13267197b363b6cfff82fafb9838754
Dilan <ramadhanrizqi21@gmail.com>
2020-10-20 22:50:49 +0700

    latihan 6

---
+++ src/latihan6kambingstatic/Mamalia.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan6kambingstatic;
+
+/**
+ *
+ * @author Muhammad Alvin
+ */
+public class Mamalia {
+    //Variable jumlahKambing dideklarasi kan sebagai statik
+    public static int jumlahKambing;
+}
